28bf02b022c4bfe6c96b11b2d0a18accda654b76
Arthur Nascimento <nascimentoarthur.br@gmail.com>
2023-12-06 17:46:35 -0300

    main

--- leituraApp/app_leitura/models.py
+++ leituraApp/app_leitura/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+	name         = models.CharField(max_length=60)
+	writter      = models.CharField(max_length=60)
+	pages_num    = models.IntegerFild()
+	release_date = models.DateFild()
+
+
+class Client(models.Model):
+	username = models.CharField(max_length=60)
+	age      = models.IntegerFild()
