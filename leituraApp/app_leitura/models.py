90c54ec72a79339de95f77b57f23a4a7ada856e5
Arthur Nascimento <101121996+NascimentoArthur@users.noreply.github.com>
2023-12-11 14:45:56 -0300

    Ajuste PR

--- leituraApp/app_leitura/models.py
+++ leituraApp/app_leitura/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Book(models.Model):
-	name         = models.CharField(max_length=60)
-	writter      = models.CharField(max_length=60)
-	pages_num    = models.IntegerFild()
-	release_date = models.DateFild()
+class Livro(models.Model):
+	nomeLivro			   = models.CharField(max_length=60)
+	quantidadePaginas      = models.IntegerFild()
+	categoria              = models.CharField(max_length=60)
+	paginaAtual            = models.IntegerFild()
+	concluido			   = models.BooleanField(false)
 
 
 class Client(models.Model):
