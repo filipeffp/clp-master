7e0bbc9b51ac59d9bdd8b9909efbc8f878f5f430
a.nascimento.moura <a.nascimento.moura@accenture.com>
2024-01-22 16:07:36 -0300

    nascimentoarthur_dev

---
+++ leituraApp/api/models/Historico.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.db import connection
+
+
+class Historico(models.Model):
+    historico_id    = models.BigAutoField(primary_key=True)
+    usuario_id      = models.IntegerField()
+    livro_id        = models.IntegerField()
+    data_leitura    = models.DateTimeField()
+    pagina_atual    = models.IntegerField()          
+    data_meta       = models.DateTimeField()
+
+    def __str__(self):
+        return self.historico_id
+
+    def metaTerminoDeLeitura(self, nova_data):
+        self.data_meta = nova_data
+        self.save
+        return f"Data de meta para fim da leitura atualizada com sucesso. Nova data é: {self.data_meta}"
+    
+        
