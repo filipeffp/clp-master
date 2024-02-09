59d75bf8df95a874b212291b8d75eb98b70c5238
elvinofabio <88414297+elvinofabio@users.noreply.github.com>
2024-02-09 15:16:54 -0300

    ajuste

--- leituraApp/config/settings.py
+++ leituraApp/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)!26l(19x1q#lo#!m@8qcymaw0#2(ga)evh263b%q4#mly3jc0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
